3adeee2e51e7831d9db65a59ff95eab79b239eca
Amanda Duffy <arduffy26@yahoo.com>
2018-04-09 12:26:46 -0500

    starting the DarkSkiApi

---
+++ app/src/main/java/com/bignerdranch/android/JournalPrime/DarkSkyApi.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.JournalPrime;
+
+/**
+ * Created by ad939564 on 4/9/2018.
+ */
+
+public class DarkSkyApi {
+
+    private static final String API ="https://api.darksky.net/forecast/0608051a6da549bf10d568618a505a37";
+
+}
