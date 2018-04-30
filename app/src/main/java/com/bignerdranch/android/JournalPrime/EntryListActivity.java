5442c0e5ad38457f046638e7d2976d2c98a77d1e
Amanda Duffy <arduffy26@yahoo.com>
2018-04-30 10:38:44 -0500

    started master/detail-not complete

--- app/src/main/java/com/bignerdranch/android/JournalPrime/EntryListActivity.java
+++ app/src/main/java/com/bignerdranch/android/JournalPrime/EntryListActivity.java
@@ -1,11 +1,32 @@
 package com.bignerdranch.android.JournalPrime;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class EntryListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
         return new EntryListFragment();
+    }
+
+    @Override
+    protected int getLayoutResId(){
+        return R.layout.activity_masterdetail;
     }
 }
